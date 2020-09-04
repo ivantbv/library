bdb3d11784b3243dd6d61e70df63528102312026
Ivan <ivantbv@gmail.com>
2020-09-04 22:16:59 +0500

    added a form that takes user input and adds it to the DOM. User input is stored in an array of objects

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let myLibrary = [];
-const addBtn = document.querySelector('#book-btn')
+let btnClicked = 0;
+const bookAdd = document.querySelector('.add');
+
+const container = document.querySelector('#book-card');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -12,22 +15,63 @@ function Book(title, author, pages, read) {
     return `${this.title} by ${this.author}, ${this.pages} pages long. Is read - ${this.read}`
 }
 
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  let addTitle = document.querySelector('#title')
+  let addAuthor = document.querySelector('#author');
+  let addPages = document.querySelector('#pages');
+  let addRead = document.querySelectorAll('input[name="read"]')
+  let selectedValue
+
 function addBookToLibr() {
-    addBtn.addEventListener('click', function() {
-        //let addBook = prompt('Enter the title of the book');
+    bookAdd.addEventListener('click', function() {
 
         //for (let i = 0; i <= 4; i++) {
-            let addTitle = prompt('Enter the title of the book')
-            let addAuthor = prompt('Enter the author of the book');
-            let addPages = prompt('Number of pages?')
-            let addRead = prompt('Did you finish reading it?')
-           let info = new Book(`${addTitle}`, `${addAuthor}`, `${addPages}`, `${addRead}`);
+           
+            for (const radioBtn of addRead) {
+                if (radioBtn.checked) {
+                    selectedValue = radioBtn.value;
+                    break;
+                }
+            }
 
-           myLibrary.push(info);
+           let inf = new Book(`${addTitle.value}`, `${addAuthor.value}`, `${addPages.value}`, `${selectedValue}`);
+
+           myLibrary.push(inf);
+           console.log(inf);
         console.log(myLibrary);
-        
+        console.log(inf.info)
+        btnClicked = 1;
+
+        // for (let property in inf) {
+        //     console.log(property + '=' + inf[property])
+        // }
+
+        function displayBooks() {
+            if (myLibrary.length > 0) {
+
+                    const div = document.createElement('div');
+                    div.className += 'book-divs'
+                    div.textContent = `${inf.title} | ${inf.author} | ${inf.pages} | ${inf.read}`;
+                    console.log(div);
+                    container.appendChild(div);
+        }
+        }
+        displayBooks();
     })
     //can take user's input
     //can store the new Book object into myLibrary
 }
 addBookToLibr();
+
+// if (btnClicked > 0) {
+//     addTitle.value = '';
+//     addAuthor.value = '';
+// }
+
