2c9ed4cd671b49336eeeedda0bc845ff9e06da31
Ivan <ivantbv@gmail.com>
2020-09-06 21:46:38 +0500

    styling the cards - the toggle button and remove book buttons. JS - added a cap for the number input and uppercased the book titles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [];
+const myLibrary = [];
 let btnClicked = false;
 const bookAdd = document.querySelector('.add');
 
@@ -15,17 +15,6 @@ function Book(title, author, pages, read) {
     return `${this.title} ${this.author} ${this.pages} ${this.read}`
 }
 
-Book.prototype.readStatus = function(toggled) {
-    return this.read
-}
-
-const toggleStatus = new Book()
-
-// Add a button on each book’s display to change its read status.
-//To facilitate this you will want to create the function that toggles a 
-//book’s read status on your Book prototype instance.
-
-
 function openForm() {
     document.getElementById("myForm").style.display = "block";
   }
@@ -35,12 +24,10 @@ function openForm() {
   }
 
   let addTitle = document.querySelector('#title')
-  let addAuthor = document.querySelector('#author');
-  let addPages = document.querySelector('#pages');
-  let addRead = document.querySelectorAll('input[name="read"]')
+  const addAuthor = document.querySelector('#author');
+  const addPages = document.querySelector('#pages');
+  const addRead = document.querySelectorAll('input[name="read"]')
   let selectedValue
-
-
 
 function addBookToLibr() {
     bookAdd.addEventListener('click', function() {
@@ -51,7 +38,7 @@ function addBookToLibr() {
                     break;
                 }
             }
-           let inf = new Book(`Book title: ${addTitle.value}`, `Author: ${addAuthor.value}`, `Pages Count: ${addPages.value}`, `Read status: ${selectedValue}`);
+           let inf = new Book(`${addTitle.value}`, `by ${addAuthor.value}`, `${addPages.value} pages`, `Status: ${selectedValue}`);
 
            myLibrary.push(inf);
 
@@ -60,43 +47,56 @@ function addBookToLibr() {
         // }
 
         function displayBooks() {
-            if (addAuthor.value != '' || addTitle.value != '' || addPages.value != '') {
+            if (addAuthor.value != '' && addTitle.value != '' && addPages.value != '') {
                     const div = document.createElement('div');
                     const divAuthor = document.createElement('div');
                     const divPages = document.createElement('div');
                     const divRead = document.createElement('div');
 
-                    div.className += 'book-divs'
-                    div.textContent = `${inf.title}` 
+                    const bookCard = document.createElement('div');
+                    bookCard.classList.add('styleCards');
+
+                    div.className += 'toBeStyled'
+                    div.textContent = `${inf.title.toUpperCase()}` 
                     
                     divAuthor.className += 'book-divs';
-                    divAuthor.textContent = `${inf.author}` 
+                    divAuthor.textContent = `${titleCase(inf.author)}` 
                     
                     divPages.className += 'book-divs'
                     divPages.textContent = `${inf.pages}` 
                     
-                    divRead.className += 'book-divs'; 
+                    //divRead.className += 'book-divs'; 
+                    divRead.className += 'statusRead'
                     divRead.textContent = `${inf.read}`;
 
-                    container.appendChild(div);
-                    container.appendChild(divAuthor);
-                    container.appendChild(divPages);
-                    container.appendChild(divRead);
+                    bookCard.appendChild(div);
+                    bookCard.appendChild(divAuthor);
+                    bookCard.appendChild(divPages);
+                    bookCard.appendChild(divRead);
+                    container.appendChild(bookCard);
+
+                    // container.appendChild(div);
+                    // container.appendChild(divAuthor);
+                    // container.appendChild(divPages);
+                    // container.appendChild(divRead);
+
                     
                     const removeBtn = document.createElement('button')
                     const toggleBtn = document.createElement('button');
                     toggleBtn.textContent = 'Toggle Status';
+                    toggleBtn.className += 'toggleBtn'
                     removeBtn.className += 'removeBtn'
                     removeBtn.textContent = 'X';
                     const blankDiv = document.createElement('div');
                     container.appendChild(blankDiv);
 
-                    
-                    blankDiv.appendChild(removeBtn);
-                    blankDiv.append(toggleBtn);
+                    container.appendChild(removeBtn);
+                    container.append(toggleBtn);
 
-                    container.style.cssText = 'border: 1px solid black;'
-
+                    blankDiv.style.cssText = 'display: flex; align-items: center;'
+                    //removeBtn.style.cssText = ''
+                    //bookCard.style.cssText = ''
+                toggleBtn.style.cssText = 'position: relative; top: -80px; left: 132px;'
                     removeBtn.addEventListener('click', () => {
                     //    div.removeChild(div.firstChild)
                     //    div.removeChild(removeBtn);
@@ -105,37 +105,60 @@ function addBookToLibr() {
                        divPages.remove();
                        divRead.remove();
                        blankDiv.remove();
-                       
+                       bookCard.remove();
+
+                       toggleBtn.remove();
+                       removeBtn.remove()
                        //container.style.cssText = '';
                     });
 
                     toggleBtn.addEventListener('click', () => {
-                        if (divRead.textContent == 'Read status: Read') {
-                            divRead.textContent = 'Read status: Not read';
-                        } else if (divRead.textContent == 'Read status: Not read') {
-                            divRead.textContent = 'Read status: In progress'
-                        } else if (divRead.textContent == 'Read status: In progress') {
-                            divRead.textContent = 'Read status: Read'
+                        if (divRead.textContent == 'Status: Read') {
+                            divRead.textContent = 'Status: Not read';
+                        } else if (divRead.textContent == 'Status: Not read') {
+                            divRead.textContent = 'Status: In progress'
+                        } else if (divRead.textContent == 'Status: In progress') {
+                            divRead.textContent = 'Status: Read'
                         }
-                    })
-                    
+                    })  
         }
 
         }
         displayBooks();
 
-        if (addTitle.value.length > 0 || addAuthor.value.length > 0 || addPages.value.length > 0) {
+        if (addTitle.value.length > 0 && addAuthor.value.length > 0 && addPages.value.length > 0) {
             addAuthor.value = '';
             addTitle.value = '';
             addPages.value = '';
         }
-
-   
     })
-    //can store the new Book object into myLibrary
 }
 addBookToLibr();
 
+//cap max length on number input
+function maxLengthCheck(object) {
+    if (object.value.length > object.maxLength)
+      object.value = object.value.slice(0, object.maxLength)
+  }
+    
+  function isNumeric (evt) {
+    let theEvent = evt || window.event;
+    let key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode (key);
+    let regex = /[0-9]|\./;
+    if ( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+  }
+
+  function titleCase(str) {
+    let splitStr = str.toLowerCase().split(' ');
+    for (let i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+    }
+    return splitStr.join(' '); 
+ }
 
 // function btnDisabled(btn) {
 //    //return  !addTitle.value.length || !addAuthor.value.length || !addPages.value.length ?   bookAdd.disabled = true :  bookAdd.disabled = false;
